580d7dc6815a672530c2e687dd871d391fe4b2b5
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-01-27 14:01:33 +0100

    update

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -3,13 +3,13 @@ const bcrypt = require('bcryptjs');
 const { User } = require('../../models');
 
 const register = async (req, res) => {
-  const { name, logName, password } = req.body;
+  const { name, logName, password, station } = req.body;
   const user = await User.findOne({ logName });
   if (user) {
     throw new Conflict(`User with ${logName} already exist`);
   }
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  await User.create({ name, logName, password: hashPassword });
+  await User.create({ name, logName, password: hashPassword, station });
   res.status(201).json({
     status: 'success',
     code: 201,
@@ -17,6 +17,7 @@ const register = async (req, res) => {
       user: {
         name,
         logName,
+        station,
       },
     },
   });
