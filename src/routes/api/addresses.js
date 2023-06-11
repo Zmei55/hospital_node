6d9ea902e6ceaaabdae88da45a656033ed094ab8
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-06-11 21:37:19 +0200

    addresses update

--- src/routes/api/addresses.js
+++ src/routes/api/addresses.js
@@ -9,18 +9,18 @@ const router = express.Router();
 router.post('/add', auth, ctrlWrapper(ctrl.add));
 
 //? GET address by id
-router.post('/add', auth, ctrlWrapper(ctrl.getById));
+router.get('/:id', auth, ctrlWrapper(ctrl.getById));
 
 //? POST address search by filter
-router.post('/add', auth, ctrlWrapper(ctrl.getByData));
+router.post('/', auth, ctrlWrapper(ctrl.getByData));
 
 //? GET all addresses
 router.get('/', auth, ctrlWrapper(ctrl.getAll));
 
 //? PUT update address
-router.post('/add', auth, ctrlWrapper(ctrl.updateById));
+router.put('/:id', auth, ctrlWrapper(ctrl.updateById));
 
 //? DELETE address by id
-router.post('/add', auth, ctrlWrapper(ctrl.removeById));
+router.delete('/:id', auth, ctrlWrapper(ctrl.removeById));
 
 module.exports = router;
