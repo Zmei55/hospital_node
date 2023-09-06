b21a3f8abd7055a6aa5545ecc45032cd415f079c
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-09-06 10:22:46 +0200

    service update for FormData

--- src/controllers/services/getByName.js
+++ src/controllers/services/getByName.js
@@ -4,16 +4,10 @@ const { Service } = require('../../models');
 const getByName = async (req, res) => {
   const { filter } = req.body;
 
-  let result = [];
-
-  if (filter === '') {
-    result = await Service.find({ isActive: true });
-  } else {
-    result = await Service.find(
-      { $and: [{ name: { $regex: filter } }, { isActive: true }] },
-      '-createdAt -updatedAt'
-    );
-  }
+  const result = await Service.find(
+    { $and: [{ name: { $regex: filter } }, { isActive: true }] },
+    '-createdAt -updatedAt'
+  );
 
   if (result.length === 0) {
     throw new NotFound(`Services with name=${filter} not found. Node`);
